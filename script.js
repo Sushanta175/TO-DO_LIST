83d6cb88d019634c16529b38bba2859d59bdabfa
Sushanta Sahu <sushantasahu175@gmail.com>
2024-08-06 11:53:54 +0530

    add save data and showTask fuction and fix some bugs

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask() {
+function addTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
-    }else{
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.Value;
-        listContainer.appendChild(li);
-        let span =document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span)
     }
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        saveData();
+    }
+    inputBox.value = '';
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
-        e.target.ParentElement.remove();
+        e.target.parentElement.remove();
+        saveData();
     }
 }, false);
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
